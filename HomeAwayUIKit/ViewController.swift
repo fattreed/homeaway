ae7bcd516279fbfc410943be515ab6a386b0740d
matty <mattreed1890@gmail.com>
2020-03-07 11:43:55 -0600

    Initial Commit

---
+++ HomeAwayUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeAwayUIKit
+//
+//  Created by matty on 3/7/20.
+//  Copyright © 2020 matty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
